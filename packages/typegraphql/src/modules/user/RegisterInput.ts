a7c321c01db9bef9703063e7ca2de4433da71b34
TerZhang <terzhang@hotmail.com>
2020-05-17 22:51:00 -0400

    feat: add class validation for RegisterInput

--- packages/typegraphql/src/modules/user/RegisterInput.ts
+++ packages/typegraphql/src/modules/user/RegisterInput.ts
@@ -1,10 +1,21 @@
 // import { MaxLength, Length, ArrayMaxSize, Min, Max } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
+import { Length, IsEmail } from 'class-validator';
 
 @InputType()
 export class RegisterInput {
-  @Field() firstName: string;
-  @Field() lastName: string;
-  @Field() email: string;
-  @Field() password: string;
+  @Field()
+  @Length(1, 255)
+  firstName: string;
+
+  @Field()
+  @Length(1, 255)
+  lastName: string;
+
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  password: string;
 }
