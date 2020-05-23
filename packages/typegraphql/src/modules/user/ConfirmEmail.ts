13cce436fd62a19a2bf1c6836efdf37bc80ffa38
TerZhang <terzhang@hotmail.com>
2020-05-23 17:03:08 -0400

    fix: forgot to add confirmEmailPrefix

--- packages/typegraphql/src/modules/user/ConfirmEmail.ts
+++ packages/typegraphql/src/modules/user/ConfirmEmail.ts
@@ -1,6 +1,7 @@
 import { Resolver, Arg, Mutation } from 'type-graphql';
 import { User } from '../../entity/User';
 import { redis } from '../../database/redis';
+import { confirmEmailPrefix } from '../../constants/redisPrefixes';
 
 // resolve mutation for mutation to confirm email
 @Resolver()
@@ -8,7 +9,7 @@ export class ConfirmEmailResolver {
   @Mutation(() => Boolean)
   async confirmEmail(@Arg('uniqueId') uniqueId: string): Promise<Boolean> {
     // given the uniqueId key, check if its corresponding value exist in the redis storage
-    const userId = await redis.get(uniqueId);
+    const userId = await redis.get(confirmEmailPrefix + uniqueId);
     // return false if id is bad or expired
     if (!userId) return false;
 
