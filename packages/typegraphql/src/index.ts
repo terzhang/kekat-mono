b0e04c9ca4f89729135ce2514c010f1582262b50
TerZhang <terzhang@hotmail.com>
2020-06-29 20:36:51 -0400

    feat: Add query complexity gate

--- packages/typegraphql/src/index.ts
+++ packages/typegraphql/src/index.ts
@@ -1,7 +1,7 @@
+import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import Express from 'express';
 
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import session from 'express-session';
 import { redis } from './database/redis';
@@ -10,20 +10,79 @@ import cors from 'cors';
 import { SESSION_SECRET } from './env/secrets';
 import { COOKIE_NAME } from './constants/names';
 import { createGqlSchema } from './utils/createGqlSchema';
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, DocumentNode } from 'graphql';
+import { getComplexity } from 'graphql-query-complexity/dist/QueryComplexity';
+import {
+  fieldExtensionsEstimator,
+  simpleEstimator,
+} from 'graphql-query-complexity/dist/estimators';
 
 const PORT = 8000;
+
+interface operationOptions {
+  request: {
+    operationName?: string | undefined;
+    variables?: Object | undefined;
+  };
+  document: DocumentNode;
+}
+
+const queryComplexityPlugin = (schema: GraphQLSchema) => ({
+  requestDidStart: () => ({
+    didResolveOperation({ request, document }: operationOptions) {
+      /**
+       * This provides GraphQL query analysis to be able to react on complex queries to your GraphQL server.
+       * This can be used to protect your GraphQL servers against resource exhaustion and DoS attacks.
+       * More documentation can be found at https://github.com/ivome/graphql-query-complexity.
+       */
+      const complexity = getComplexity({
+        // Our built schema
+        schema,
+        // To calculate query complexity properly,
+        // we have to check only the requested operation
+        // not the whole document that may contains multiple operations
+        operationName: request.operationName,
+        // The GraphQL query document
+        query: document,
+        // The variables for our GraphQL query
+        variables: request.variables,
+        // Add any number of estimators. The estimators are invoked in order, the first
+        // numeric value that is being returned by an estimator is used as the field complexity.
+        // If no estimator returns a value, an exception is raised.
+        estimators: [
+          // Using fieldExtensionsEstimator is mandatory to make it work with type-graphql.
+          fieldExtensionsEstimator(),
+          // Add more estimators here...
+          // This will assign each field a complexity of 1
+          // if no other estimator returned a value.
+          simpleEstimator({ defaultComplexity: 1 }),
+        ],
+      });
+      const maxComplexity = 20;
+      // Here we can react to the calculated complexity,
+      // like compare it with max and throw error when the threshold is reached.
+      if (complexity > maxComplexity) {
+        throw new Error(
+          `Query is too complicated! ${complexity} is over ${maxComplexity} that is the max allowed complexity.`
+        );
+      }
+      // And here we can e.g. subtract the complexity point from hourly API calls limit.
+      console.log('Used query complexity points:', complexity);
+    },
+  }),
+});
 
 // this make it start async'ly
 const main = async () => {
   // https://typeorm.io/#/connection/creating-a-new-connection
   // this read from orm config to make a connection to database
   await createConnection();
-
+  const schema = (await createGqlSchema()) as GraphQLSchema;
   // this build a graphQL schema to be used by the server
   const apolloServer = new ApolloServer({
-    schema: (await createGqlSchema()) as GraphQLSchema,
+    schema,
     context: ({ req, res }: any) => ({ req, res }), // access to the request and respond object
+    plugins: [queryComplexityPlugin(schema)],
   });
 
   // configuring redisStore with session
