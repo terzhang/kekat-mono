9756bdc31f46dda38b727cf05fb263dc07c2063d
TerZhang <terzhang@hotmail.com>
2020-07-08 21:17:45 -0400

    refactor: Login resolver now returns Error

--- packages/typegraphql/src/modules/user/Login.resolver.ts
+++ packages/typegraphql/src/modules/user/Login.resolver.ts
@@ -1,46 +1,31 @@
-import {
-  Resolver,
-  Query,
-  Arg,
-  Mutation,
-  Ctx,
-  UseMiddleware,
-} from 'type-graphql';
+import { Resolver, Arg, Mutation, Ctx } from 'type-graphql';
 import bcrypt from 'bcryptjs';
 import { User } from '../../entity/User';
 import { LoginInput } from './LoginInput';
 import { Context } from '../../types/context';
-import { isAuth } from '../../middlewares/isAuth';
 
-/* Resolvers */
+/** handle user logins */
 // Providing an object type as arg to the Resolver decorator will...
 // allow this class to resolve a field within that object type
 // (in this case, the fullName field within User entity)
 @Resolver(User)
 export class LoginResolver {
-  // GET request for a recipe via id
-  @UseMiddleware(isAuth)
-  @Query(() => String) // assign a name for the query must be camelCase
-  async getUser(@Arg('id') id: string) {
-    return 'The id: ' + id + ' is now registered';
-  }
-
   // generate a new User modelled by the User entity
   // tells both typeGraphql and typescript that it...
   // returns a promise that gives back a User entity object
-  @Mutation(() => User, { nullable: true })
+  @Mutation(() => User)
   async login(
     @Arg('data')
     { email, password }: LoginInput,
     @Ctx() ctx: Context // this gets the req object from context
-  ): Promise<User | null> {
+  ): Promise<User | Error> {
     // try to find user, if can't return null
     const user: User | undefined = await User.findOne({ where: { email } });
-    if (!user) return null;
+    if (!user) throw new Error('Incorrect login info');
 
     // user exist, then compare password to check if it's valid
     const valid = await bcrypt.compare(password, user.password);
-    if (!valid) return null;
+    if (!valid) throw new Error('Incorrect login info');
 
     // set the user id inside the request's session cookie
     // controlled by the session middleware
