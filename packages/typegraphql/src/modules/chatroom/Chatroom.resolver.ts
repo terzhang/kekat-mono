72292c63a0c5581d0e4ab2ff3138aa932c14500b
TerZhang <terzhang@hotmail.com>
2020-07-08 21:07:22 -0400

    feat: add chatroom resolver

--- packages/typegraphql/src/modules/chatroom/Chatroom.resolver.ts
+++ packages/typegraphql/src/modules/chatroom/Chatroom.resolver.ts
@@ -1,12 +1,46 @@
-import { Resolver, Query, Arg } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation, Ctx } from 'type-graphql';
 import { Chatroom } from '../../entity/Chatroom';
+import { UserChatroom } from '../../entity/UserChatroom';
+import { Context } from '../../types/context';
 
-@Resolver()
+@Resolver(Chatroom)
 export class ChatroomResolver {
-  @Query((_type) => [Chatroom], { nullable: true })
-  async getChatrooms(@Arg('limit') limit?: number): Promise<Chatroom[] | null> {
+  /** resolve query to get all the chatrooms within the database */
+  @Query((_type) => [Chatroom])
+  async getAllChatrooms(
+    @Arg('limit') limit: number
+  ): Promise<Chatroom[] | Error> {
     const chatrooms = await Chatroom.find({ take: limit });
-    if (!chatrooms) return null;
+    if (!chatrooms) throw new Error('No chatroom exists');
     return chatrooms;
+  }
+
+  /** resolve mutation to add self to a room */
+  @Mutation((_type) => Chatroom)
+  async joinChatroom(
+    @Arg('chatroomId') chatroomId: string,
+    @Ctx() context: Context
+  ): Promise<Chatroom | Error> {
+    // make sure chatroom exists FIRST
+    const chatroom = await Chatroom.findOne({ id: chatroomId });
+    if (!chatroom)
+      throw new Error('The chatroom you are joining does not exist');
+
+    // ? check if user is already in the chatroom by looking for it
+    const userExist = await UserChatroom.findOne({
+      userId: context.req.session!.userId,
+      chatroomId,
+    });
+    if (userExist) throw new Error('You have already joined the chatroom.');
+
+    // create the relationship between user and chatroom
+    const userChatroom = await UserChatroom.create({
+      chatroomId,
+      // ! it's userId not id
+      userId: context.req.session!.userId,
+    }).save();
+    if (!userChatroom) throw new Error('Could not join room');
+
+    return chatroom;
   }
 }
