5bad7337753ee6f8f560b9f7b4d8372620f55170
TerZhang <terzhang@hotmail.com>
2020-07-23 14:14:03 -0400

    feat: update client url

--- packages/typegraphql/src/constants/urlPrefixes.ts
+++ packages/typegraphql/src/constants/urlPrefixes.ts
@@ -1,5 +1,4 @@
-export const forgotPasswordUrlPrefix =
-  'http://localhost:9999/user/change-password/';
-
-export const confirmEmailUrlPrefix =
-  'http://localhost:9999/user/confirm-email/';
+// TODO: make clientUrl conditional depending on dev or prod env
+export const clientUrl = 'http://localhost:8888';
+export const forgotPasswordUrlPrefix = clientUrl + '/user/change-password/';
+export const confirmEmailUrlPrefix = clientUrl + '/user/confirm-email/';
