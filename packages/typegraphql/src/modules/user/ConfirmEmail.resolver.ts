574e540a0fd8e21104bdd8a5c839edb9a0b6944e
TerZhang <terzhang@hotmail.com>
2020-07-24 05:59:21 -0400

    feat: resolver that send a new confirmation email

--- packages/typegraphql/src/modules/user/ConfirmEmail.resolver.ts
+++ packages/typegraphql/src/modules/user/ConfirmEmail.resolver.ts
@@ -2,10 +2,13 @@ import { Resolver, Arg, Mutation } from 'type-graphql';
 import { User } from '../../entity/User';
 import { redis } from '../../database/redis';
 import { confirmEmailPrefix } from '../../constants/redisPrefixes';
+import { sendConfirmationEmail } from '../../utils/sendMail';
+import { confirmEmailUrlPrefix } from '../../constants/urlPrefixes';
+import { confirmationUrl } from '../../utils/confirmationUrl';
 
-// resolve mutation for mutation to confirm email
 @Resolver()
 export class ConfirmEmailResolver {
+  /** resolve for mutation to confirm email */
   @Mutation(() => Boolean)
   async confirmEmail(@Arg('uniqueId') uniqueId: string): Promise<Boolean> {
     // given the uniqueId key, check if its corresponding value exist in the redis storage
@@ -23,6 +26,31 @@ export class ConfirmEmailResolver {
       return false;
     }
 
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async sendConfirmEmail(@Arg('email') email: string): Promise<true> {
+    // verify email exist
+    const user = await User.findOne({ where: { email } });
+    if (!user || user.confirmed) return true;
+
+    // TODO: make sure user don't have another confirmation token active already
+    // generate confirm url and set token in redisu
+    const url = await confirmationUrl({
+      userId: user.id,
+      prefix: confirmEmailPrefix,
+      urlPrefix: confirmEmailUrlPrefix,
+    });
+
+    // safe guard in case url can't be generated or token can't be set in redis
+    if (!url) {
+      console.log('Failed to set token in Redis');
+      throw new Error('Server is experiencing issues. Please try again later.');
+    }
+
+    // TODO: find out if nodemailer throws any error
+    await sendConfirmationEmail(email, url);
     return true;
   }
 }
