24d1de95a3d000e94b5cca6838d7d1313322d3ce
TerZhang <terzhang@hotmail.com>
2020-07-27 18:42:28 -0400

    feat: emit schema SDL every schema build

--- packages/typegraphql/src/utils/createGqlSchema.ts
+++ packages/typegraphql/src/utils/createGqlSchema.ts
@@ -1,5 +1,9 @@
+import path from 'path';
 import { buildSchema } from 'type-graphql';
 import { userAuthChecker } from './AuthChecker';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase();
+const isTesting = NODE_ENV === 'test' || NODE_ENV == 'testing';
 
 /** helper function to promise building a typegraphql Schema */
 export const createGqlSchema = () =>
@@ -7,4 +11,12 @@ export const createGqlSchema = () =>
   buildSchema({
     resolvers: [__dirname + '/../modules/**/*.resolver.ts'],
     authChecker: userAuthChecker,
+    // generate schema SDL every schema build
+    emitSchemaFile: !isTesting
+      ? {
+          path: path.resolve(__dirname, '../schema/schema.gql'),
+          commentDescriptions: true,
+          sortedSchema: false, // by default the printed schema is sorted alphabetically
+        }
+      : undefined,
   });
